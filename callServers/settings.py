ae903d2110cdd3f61b5f064d46e2c65ff0217e87
Planet Bits <sarnobatps@gmail.com>
2024-12-07 15:24:47 +0530

    Update settings.py

--- callServers/settings.py
+++ callServers/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_8mq4=f@+dilz+dmt(pjldxj5+2(6!u$@wu(h&28c#m5tlf7=5'
+SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
